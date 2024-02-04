32609cbb0690fd214f68eda2b777270fd57f2043
Mr. Sanjoy <--add>
2024-02-04 16:38:21 +0530

    postgres is conected

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 import express from 'express';
+import pg from 'pg';
+const { Client, Pool } = pg;
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 
-app.get('/', (req, res) => res.send('Hello World'))
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    password: 'sanjoypql',
+    database: 'postgres',
+    port: 5432
+});
+
+
+pool.query('SELECT NOW ()', (err, res) => {
+    if (err) {
+        console.error("Error connecting to PostgreSQL: ", err);
+    } else {
+        console.log('Connected to PostgreSQL:', res.rows[0].now);
+    }
+});
+
+app.get('/', (req, res) => res.send('Hello World'));
 
 app.listen(PORT, () => {
     console.log(`Express server listening on port http://localhost:${PORT} `);
